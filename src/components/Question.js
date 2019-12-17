335743987cec9b0902b67d640a145593eb9ee549
Christopher <christopher@seekingwebdev.com>
2019-12-17 12:07:24 +0000

    add colors to checker funcionality

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,16 +4,21 @@ export default function Question(props) {
 
   let correct = {isCorrect : true, answer : props.question.correct_answer}
   let incorrect = props.question.incorrect_answers.map(answer => ({ isCorrect : false , answer : answer }))
-  
+
   function shuffle (array) {
-    return array.sort(() => Math.random() - 0.5)
-  }  
+    return array.sort(() => Math.random() - 0.5);
+  }
 
   // putting answers into to a new single answers array with a spread operator as objects so we can identify which is correct
-  let answers = shuffle([correct, ...incorrect])
+  let answers = shuffle([correct, ...incorrect]);
 
   function checkAnswer (e) {
-    console.log(e.target.id)
+    if (e.target.id === "true") {
+      e.target.style.backgroundColor = "green";
+    }
+    if (e.target.id === "false") {
+      e.target.style.backgroundColor = "red";
+    }
   }
 
   return (
@@ -21,12 +26,10 @@ export default function Question(props) {
       <p>{props.question.question}</p>
 
       {answers.map((answer, index) => (
-        <p id={answer.isCorrect.toString()} onClick={checkAnswer} key={index}>{answer.answer}</p>
+        <p id={answer.isCorrect.toString()} className={answer.answer} style={{backgroundColor: "white"}} onClick={checkAnswer} key={index}>{answer.answer}</p>
       ))}
-      {/* <p>{props.question.correct_answer}</p>
-      <p>{props.question.incorrect_answers[0]}</p>
-      <p>{props.question.incorrect_answers[1]}</p>
-      <p>{props.question.incorrect_answers[2]}</p> */}
+
+      <p className="answer"></p>
     </div>
   )
 }
