9510c163d3e19490acbc004697f5baf3d919f9a8
Yynx <47715709+Yynx@users.noreply.github.com>
2019-12-17 09:45:30 +0000

    Add function for randomising answers

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 export default function Question(props) {
+
+  let correct = {isCorrect : true, answer : props.question.correct_answer}
+  
+  let incorrect = props.question.incorrect_answers.map(answer => ({ isCorrect : false , answer : answer }))
+
+  let answers = [correct, ...incorrect]
+
+  console.log(answers)
+
+  function shuffle (array) {
+    return array.sort(() => Math.random() - 0.5)
+  }
+
+  let array = [1 , 2 , 3, 4]
+
+
   return (
     <div>
       <p>{props.question.question}</p>
@@ -8,6 +24,7 @@ export default function Question(props) {
       <p>{props.question.incorrect_answers[0]}</p>
       <p>{props.question.incorrect_answers[1]}</p>
       <p>{props.question.incorrect_answers[2]}</p>
+      {    console.log(shuffle(array))}
     </div>
   )
 }
