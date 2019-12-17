a4e6ef4df18797fe49a3ff89f4cf94525d019b77
Christopher <christopher@seekingwebdev.com>
2019-12-17 20:48:32 +0000

    change Question to class component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import Answer from './Answer';
 
-export default function Question(props) {
-
-  let correct = {isCorrect : true, answer : props.question.correct_answer}
-  let incorrect = props.question.incorrect_answers.map(answer => ({ isCorrect : false , answer : answer }))
-
-  function shuffle (array) {
-    return array.sort(() => Math.random() - 0.5);
+export default class Question extends React.Component {
+  state = {
+    answers: this.shuffle([{isCorrect : true, answer : this.props.question.correct_answer}, ...this.props.question.incorrect_answers.map(answer => ({ isCorrect : false , answer : answer }))])
   }
-
   // putting answers into to a new single answers array with a spread operator as objects so we can identify which is correct
-  let answers = shuffle([correct, ...incorrect]);
+
+  shuffle(array) {array.sort(() => Math.random() - 0.5)}
 
   // function checkAnswer (e) {
   //   if (e.target.id === "true") {
@@ -22,17 +18,19 @@ export default function Question(props) {
   //   }
   // }
 
-  return (
-    <div>
-      <p>{props.question.question}</p>
+  render() {
+    return (
+      <div>
+        <p>{this.props.question.question}</p>
 
-      {answers.map((answer, index) => (
-        <Answer isCorrect={answer.isCorrect.toString()}  key={index} text={answer.answer} />
-      ))}
-      <button onClick={props.button}>Next</button>
-      <p className="answer"></p>
-    </div>
-  )
+        {this.state.answers.map((answer, index) => (
+          <Answer isCorrect={answer.isCorrect.toString()}  key={index} text={answer.answer} />
+          ))}
+        <button onClick={this.props.button}>Next</button>
+        <p className="answer"></p>
+      </div>
+    )
+  }
 }
 
 // change this to a class and then add state of background
@@ -40,7 +38,7 @@ export default function Question(props) {
 // in question.js have state of background as null
 // when rendering <Answer /> set className to this.state.background (null)
 // this happens in qustion.js
-// 
+//
 
 // in answer.js don't set State but directly change background using onClick function by adding classes
 // do this by e.target.className = green OR red
