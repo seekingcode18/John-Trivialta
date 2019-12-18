494a25e561cd25fa222ddbfe02714ad91792c4e7
Christopher <christopher@seekingwebdev.com>
2019-12-18 13:52:00 +0000

    implement colour functionality

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -18,41 +18,28 @@ export default class Answer extends React.Component {
         }
     }
 
-// componentDidMount() {
-//     console.log('Answer.js componentDidMount')
-//     this.setState({background: this.props.background})
-// }
+   // In Questions and brought down through props
+   checkAnswer (e) {
+        if (e.target.id === "true") {
+            this.setState({background: "green"})
+        }
+        if (e.target.id === "false") {
+            this.setState({background: "red"})
+        }
+      }
 
+    // updateColour(e) {
+    //     // e.target.classList.add("red")
+    //     this.setState({background: "red"})
+    //     // e.target.classList.remove("white")
+    //     console.log(`updateColour(${this.state.background})`);
+    //     // e.target.className.add = "cyan";
+    // }
 
-    // removed this state as state will be initialised in question.justify
-    // make function for toggling classes green/red
-    //   which will be wiped when question.js renders new <Answers />
-
-    // In Questions and brought down through props
-//    const checkAnswer = (e) => {
-//         if (e.target.id === "true") {
-//         //   e.target.style.backgroundColor = "green";
-//         // boolean true
-//         setState({ background: true})
-//         // setState({background: "green"})
-//         }
-//         if (e.target.id === "false") {
-//         //   e.target.style.backgroundColor = "red";
-//         setState({ background: false})
-//         // setState({background: "red"})
-//         }
-//       }
-    updateColour(e) {
-        // e.target.classList.add("red")
-        this.setState({background: "red"})
-        // e.target.classList.remove("white")
-        console.log(`updateColour(${this.state.background})`);
-        // e.target.className.add = "cyan";
-    }
     render() {
         return (
             <div>
-            <p id={this.props.isCorrect} className={this.state.background} onClick={this.props.clicker} onClickCapture={this.updateColour.bind(this)}
+            <p id={this.props.isCorrect} className={this.state.background} onClick={this.props.clicker} onClickCapture={this.checkAnswer.bind(this)}
             >{this.props.text}</p>
             {/* <p>{this.props.text}</p> */}
         </div>
