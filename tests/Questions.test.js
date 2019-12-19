d620479c0da46f141ac7f269cd0473d329d28ab1
Christopher <christopher@seekingwebdev.com>
2019-12-19 12:27:24 +0000

    add test for Questions

--- tests/Questions.test.js
+++ tests/Questions.test.js
@@ -4,25 +4,17 @@ import Questions from '../src/containers/Questions';
 import Enzyme, { shallow } from 'enzyme';
 
 describe('renders Questions without crashing', () => {
-    // it('smoke test', () => {
-    //     const div = document.createElement('div');
-    //     ReactDOM.render(<Questions />, div);
-    //     ReactDOM.unmountComponentAtNode(div);
-    // })
-    // it('is there a div', () => {
-    //     const wrapper = shallow(<Questions />);
-    //     expect(wrapper.find('div')).toHaveLength(1);
-    // })
-
-    it('vapour test', () => {
+    it('smoke test', () => {
         const simulatedData = {difficulty: 'easy', category: 9};
         const div = document.createElement('div');
         ReactDOM.render(<Questions properties={simulatedData} />, div);
         ReactDOM.unmountComponentAtNode(div);
     })
+
+    it('renders Results when incrementor in state = 10', () => {
+        const simulatedData = {difficulty: 'easy', category: 9};
+        const wrapper = shallow(<Questions properties={simulatedData} />);
+        wrapper.setState({incrementor: 10});
+        expect(wrapper.find('Results')).toHaveLength(1);
+    })
 })
-
-
-
-
-
